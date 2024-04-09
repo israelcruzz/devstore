105af40f7bb3b93075c8fb29994760bbcd274d04
israelcruzz <israelcruz.contato@gmail.com>
2024-04-08 22:39:13 -0300

    feat: creating home page

---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,72 @@
+import Header from "@/components/header";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <main className="max-w-[1240px] mx-auto py-6">
+      <Header />
+
+      <div className="w-full mt-6 flex gap-6">
+        <section className="w-full bg-zinc-900 rounded-lg relative">
+          <Link href={"/"}>
+            <Image
+              src="/moletom-never-stop-learning.png"
+              width={831}
+              height={714}
+              alt=""
+              quality={100}
+            />
+          </Link>
+
+          <div className="absolute bottom-0 right-0 m-44 w-[240px] h-12 bg-black/60 border-2 border-zinc-500 rounded-full overflow-hidden flex items-center justify-center gap-3 pl-3 pr-1">
+            <h1 className="flex-1 text-sm truncate">Moletom Never Stop</h1>
+            <div className="bg-violet-500 w-[82px] h-10 flex items-center justify-center rounded-full">
+              <h1>R$129</h1>
+            </div>
+          </div>
+        </section>
+        <section className="w-[620px] flex flex-col gap-6">
+          <Link href={"/"} className="relative bg-zinc-900 rounded-lg">
+            <Image
+              src="/moletom-ai-side.png"
+              width={831}
+              height={714}
+              alt=""
+              quality={100}
+            />
+
+            <div className="absolute bottom-0 right-0 m-12 w-[240px] h-12 bg-black/60 border-2 border-zinc-500 rounded-full overflow-hidden flex items-center justify-center gap-3 pl-3 pr-1">
+              <h1 className="flex-1 text-sm truncate">Moletom Come Ai Side</h1>
+              <div className="bg-violet-500 w-[82px] h-10 flex items-center justify-center rounded-full">
+                <h1>R$129</h1>
+              </div>
+            </div>
+          </Link>
+
+          <Link href={"/"} className="relative bg-zinc-900 rounded-lg">
+            <Image
+              src="/camiseta-dowhile-2022.png"
+              width={831}
+              height={714}
+              alt=""
+              quality={100}
+            />
+
+            <div className="absolute bottom-0 right-0 m-12 w-[240px] h-12 bg-black/60 border-2 border-zinc-500 rounded-full overflow-hidden flex items-center justify-center gap-3 pl-3 pr-1">
+              <h1 className="flex-1 text-sm truncate">Camiseta Expande Mind</h1>
+              <div className="bg-violet-500 w-[82px] h-10 flex items-center justify-center rounded-full">
+                <h1>R$129</h1>
+              </div>
+            </div>
+          </Link>
+        </section>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import { Search, ShoppingBag } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full flex justify-between">
+      <section className="flex items-center gap-3">
+        <h1 className="text-2xl font-extrabold">devstore</h1>
+
+        <form className="flex w-[320px] items-center gap-3 px-5 py-3 rounded-full bg-zinc-900">
+          <Search className="w-5 h-5 text-zinc-500" />
+          <input
+            type="text"
+            placeholder="Buscar produtos..."
+            className="flex-1 outline-none bg-transparent placeholder:text-zinc-500 text-sm"
+          />
+        </form>
+      </section>
+
+      <section className="flex items-center gap-3">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="w-4 h-4" />
+          <h1>Cart (0)</h1>
+        </div>
+
+        <div className="w-px h-4 bg-slate-700"></div>
+
+        <Link href='/' className="flex gap-3 items-center">
+            <span className="font-medium hover:underline text-sm">Account</span>
+            <Image width={24} height={24} className="rounded-full h-6 w-6" src="/moletom-never-stop-learning.png" alt="" />
+        </Link>
+      </section>
+    </header>
+  );
+}
